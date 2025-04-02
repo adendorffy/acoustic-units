a613e11a8a7304b1a19e2a7b7ce5db208e967924
Danel Adendorff <240541055@sun.ac.za>
2025-04-02 15:23:21 +0200

    fix encoding

--- encode_my_features.py
+++ encode_my_features.py
@@ -4,31 +4,71 @@ from pathlib import Path
 import pandas as pd
 import joblib
 import argparse
-import editdistance
 import numpy as np
 from tqdm import tqdm
-from scipy.spatial.distance import cdist
+from typing import Tuple
+import numba
+import scipy.spatial.distance as distance
 
-silence_codes_wavlm_base_12 = [
-    38,
-    11,
-    1,
-    8,
-    42,
-    52,
-    60,
-    33,
-    12,
-    76,
-    81,
-    88,
-    74,
-    76,
-    33,
-    54,
-    34,
-    57,
-]
+
+def segment(
+    sequence: np.ndarray, codebook: np.ndarray, gamma: float
+) -> Tuple[np.ndarray, np.ndarray]:
+    """Group speech representations into phone-like segments.
+
+    Args:
+        sequence (NDArray): speech representations of shape (T, D) where T is the number of frames and D is the feature dimension.
+        codebook (NDArray): cluster centriods of the discrete units of shape (K, D) where K is the number of codes.
+        gamma float: Duration regularizer weight. Larger values result in a coarser segmentation.
+
+    Returns:
+        NDArray[int]: list of discrete units representing each segment sound types of shape (N,).
+        NDArray[int]: list of segment boundaries of shape (N+1,).
+    """
+    dists = distance.cdist(sequence, codebook).astype(np.float32)
+    alpha, P = _segment(dists, gamma)
+    return _backtrack(alpha, P)
+
+
+@numba.njit()
+def _segment(dists, gamma):
+    T, K = dists.shape
+
+    alpha = np.zeros(T + 1, dtype=np.float32)
+    P = np.zeros((T + 1, 2), dtype=np.int32)
+    D = np.zeros((T, T, K), dtype=np.float32)
+
+    for t in range(T):
+        for k in range(K):
+            D[t, t, k] = dists[t, k]
+    for t in range(T):
+        for s in range(t + 1, T):
+            D[t, s, :] = D[t, s - 1, :] + dists[s, :] - gamma
+
+    for t in range(T):
+        alpha[t + 1] = np.inf
+        for s in range(t + 1):
+            k = np.argmin(D[s, t, :])
+            alpha_min = alpha[s] + D[s, t, k]
+            if alpha_min < alpha[t + 1]:
+                P[t + 1, :] = s, k
+                alpha[t + 1] = alpha_min
+    return alpha, P
+
+
+@numba.njit()
+def _backtrack(alpha, P):
+    rhs = len(alpha) - 1
+    segments = []
+    boundaries = [rhs]
+    while rhs != 0:
+        lhs, code = P[rhs, :]
+        segments.append(code)
+        boundaries.append(lhs)
+        rhs = lhs
+    segments.reverse()
+    boundaries.reverse()
+    return segments
 
 
 def get_frame_num(timestamp: float, sample_rate: int, frame_size_ms: int) -> int:
@@ -45,55 +85,12 @@ def cut_encoding(
     return encoding[start_frame:end_frame]
 
 
-def collapse_runs(seq):
-    collapsed = []
-    prev = None
-    for code in seq:
-        if code != prev:
-            collapsed.append(int(code))
-            prev = code
-    return collapsed
-
-
-def distance(feat_1, feat_2):
-    length = max(len(feat_1), len(feat_2))
-    return editdistance.eval(feat_1, feat_2) / length
-
-
-def greedy_segment(
-    sequence: np.ndarray, codebook: np.ndarray, silence_codes: set = None
-) -> np.ndarray:
-    dists = cdist(sequence, codebook)
-    frame_codes = np.argmin(dists, axis=1)
-
-    segments = []
-    prev_code = frame_codes[0]
-
-    for t in range(1, len(frame_codes)):
-        if frame_codes[t] != prev_code:
-            segments.append(prev_code)
-            prev_code = frame_codes[t]
-
-    segments.append(prev_code)
-
-    if silence_codes:
-        # Remove leading silence codes
-        while segments and segments[0] in silence_codes:
-            segments.pop(0)
-
-        # Remove trailing silence codes
-        while segments and segments[-1] in silence_codes:
-            segments.pop()
-
-    return np.array(segments)
-
-
-def main(model_name: str, layer: int):
+def main(model_name: str, layer: int, gamma: float):
     model_name = model_name.upper()
     sr = 16000
     align_dir = Path("librispeech/alignments")
     align_df = pd.read_csv(align_dir / "alignments.csv")
-    features_dir = Path(f"my-features/{model_name}/{layer}")
+    features_dir = Path(f"my-features/{model_name}/{layer}/gamma{gamma}")
     features_dir.mkdir(parents=True, exist_ok=True)
 
     try:
@@ -112,49 +109,40 @@ def main(model_name: str, layer: int):
 
     kmeans_path = f"models/kmeans_{model_name.lower()}_layer{layer}_k100.pkl"
     kmeans = joblib.load(kmeans_path)
+    align_df["codes"] = None
 
-    with open("inspect.txt", "w") as f:
-        word_count = 0
-        for path in tqdm(
-            audio_paths,
-            total=len(audio_paths),
-            desc="Encoding features",
-        ):
-            wav_df = align_df[align_df["filename"] == path.stem]
-            word_count += max(wav_df["word_id"]) + 1
-            waveform, sr_loaded = torchaudio.load(str(path))
-            if sr_loaded != sr:
-                waveform = torchaudio.functional.resample(waveform, sr_loaded, sr)
+    for path in tqdm(
+        audio_paths,
+        total=len(audio_paths),
+        desc="Encoding features",
+    ):
+        wav_df = align_df[align_df["filename"] == path.stem]
+        waveform, sr_loaded = torchaudio.load(str(path))
+        if sr_loaded != sr:
+            waveform = torchaudio.functional.resample(waveform, sr_loaded, sr)
 
-            with torch.inference_mode():
-                features, _ = model.extract_features(waveform, num_layers=layer)
-                encoding = features[layer - 1].squeeze().cpu().numpy()
+        with torch.inference_mode():
+            features, _ = model.extract_features(waveform, num_layers=layer)
+            encoding = features[layer - 1].squeeze().cpu().numpy()
 
-            for w in range(1, max(wav_df["word_id"]) + 1):
-                word_df = wav_df[wav_df["word_id"] == w]
-                if word_df["text"].iloc[0] not in ["lady", "the", "a", "an"]:
-                    continue
-                cut_enc = cut_encoding(
-                    encoding,
-                    [word_df["word_start"].iloc[0], word_df["word_end"].iloc[0]],
-                )
-                codes = []
-                if len(cut_enc) > 0:
-                    codes = greedy_segment(
-                        cut_enc,
-                        kmeans.cluster_centers_,
-                    )
-                    f.write(
-                        f"{' '.join(word_df['phones'].iloc[0].split(',')):<15}[{word_df['text'].iloc[0]:<10}] | {codes}\n"
-                    )
+        for w in range(1, max(wav_df["word_id"]) + 1):
+            word_df = wav_df[wav_df["word_id"] == w]
 
-                save_path = (
-                    features_dir
-                    / path.relative_to(audio_dir).parent
-                    / f"{path.stem}_{w}.npy"
-                )
-                save_path.parent.mkdir(parents=True, exist_ok=True)
-                np.save(save_path, codes)
+            word_encoding = cut_encoding(
+                encoding,
+                [word_df["word_start"].iloc[0], word_df["word_end"].iloc[0]],
+            )
+            codes = []
+            if len(word_encoding) > 0:
+                codes = segment(word_encoding, kmeans.cluster_centers_, gamma)
+
+            save_path = (
+                features_dir
+                / path.relative_to(audio_dir).parent
+                / f"{path.stem}_{w}.npy"
+            )
+            save_path.parent.mkdir(parents=True, exist_ok=True)
+            np.save(save_path, codes)
 
 
 if __name__ == "__main__":
@@ -165,8 +153,10 @@ if __name__ == "__main__":
         default="HUBERT_BASE",
         help="Model name from torchaudio.pipelines (e.g., HUBERT_BASE, WAV2VEC2_BASE)",
     )
+    parser.add_argument("gamma", type=float, default=1.0, help="Gamma")
     parser.add_argument(
         "layer", type=int, default=8, help="Layer number to extract features from"
     )
+
     args = parser.parse_args()
-    main(args.model, args.layer)
+    main(args.model, args.layer, args.gamma)
