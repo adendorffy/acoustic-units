89b3b6aa9203e79f584165c4c02c3dd06264287f
Danel Adendorff <240541055@sun.ac.za>
2025-04-08 10:10:09 +0200

    Completed graph building with N_CLUSTERS = 50, GAMMA=0.0 and THRESHOLD = 0.4

---
+++ cluster.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+MODEL="wavlm_base"
+LAYER=7
+KMEANS_DATA_DIR="librispeech/train-clean-100"
+AUDIO_EXT=".flac"
+DATA_DIR="librispeech/dev-clean"
+ALIGN_DIR="librispeech/alignments/dev-clean"
+N_CLUSTERS=(50 100 200 300 400 500)
+# N_CLUSTERS=(200)
+GAMMA=0.0
+THRESHOLD=0.4
+RESOLUTION=0.5
+
+for n in "${N_CLUSTERS[@]}"; do
+
+    echo "🧩 Performing clustering..."
+    python cluster.py "$MODEL" "$LAYER" "$GAMMA" "$n" "features" "$THRESHOLD" "$RESOLUTION"
+
+    echo "📊 Evaluating clustering results..."
+    python evaluate.py "$MODEL" "$LAYER" "$GAMMA" "$n" "graphs" "$THRESHOLD" 
+
+
+    echo "✅ Completed iteration with N_CLUSTERS = $n!"
+
+    echo "🔄 Committing changes to Git..."
+    git add .
+    git commit -m "Completed iteration with N_CLUSTERS = $n and GAMMA = $GAMMA"
+    git push
+done
+
+echo "🎉 All experiments completed successfully!"
--- graph.sh
+++ graph.sh
@@ -6,31 +6,19 @@ KMEANS_DATA_DIR="librispeech/train-clean-100"
 AUDIO_EXT=".flac"
 DATA_DIR="librispeech/dev-clean"
 ALIGN_DIR="librispeech/alignments/dev-clean"
-N_CLUSTERS=(50 100 200 300 400 500)
-# N_CLUSTERS=(200)
+N_CLUSTERS=50
 GAMMA=0.0
-THRESHOLD=0.4
+THRESHOLDS=(0.4 0.5 0.6 0.7 0.8 0.9)
 RESOLUTION=0.5
 
-for n in "${N_CLUSTERS[@]}"; do
-
+for t in "${THRESHOLDS[@]}"; do
 
     echo "🔗 Creating graph from distances..."
-    python graph.py "$MODEL" "$LAYER" "$GAMMA" "$n" "$ALIGN_DIR" "$THRESHOLD" 
-
-    echo "🧩 Performing clustering..."
-    python cluster.py "$MODEL" "$LAYER" "$GAMMA" "$n" "features" "$THRESHOLD" "$RESOLUTION"
-
-    echo "📊 Evaluating clustering results..."
-    python evaluate.py "$MODEL" "$LAYER" "$GAMMA" "$n" "graphs" "$THRESHOLD" 
-
-
-    echo "✅ Completed iteration with N_CLUSTERS = $n!"
+    python graph.py "$MODEL" "$LAYER" "$GAMMA" "$N_CLUSTERS" "$ALIGN_DIR" "$t" 
 
     echo "🔄 Committing changes to Git..."
     git add .
-    git commit -m "Completed iteration with N_CLUSTERS = $n and GAMMA = $GAMMA"
+    git commit -m "Completed graph building with N_CLUSTERS = $N_CLUSTERS, GAMMA=$GAMMA and THRESHOLD = $t"
     git push
 done
 
-echo "🎉 All experiments completed successfully!"
